b5c1500052b9d87feee9b433e81a1f44bfbe5b67
Stephen <stephen@gilmore.us>
2022-07-05 13:34:49 -0700

    intro to html

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.post_list, name="post_list")]
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+]
