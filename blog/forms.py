4d1f86f4994fb414a7a552959e9a18cf95e0ec42
Stephen <stephen@gilmore.us>
2022-07-07 13:56:50 -0700

    done!

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
-        model = ost
+        model = Post
         fields = (
             "title",
             "text",
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -16,5 +18,14 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = postForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, "blog/post_edit.html", {"form": form})
